052fbf2cd972e8988165779c6be88f7480fc3f1f
YPares <yves.pares@gmail.com>
2013-07-01 16:04:29 +0200

    functions renamed

--- src/Control/Monad/Trans/ForkableT.hs
+++ src/Control/Monad/Trans/ForkableT.hs
@@ -14,20 +14,20 @@ runForkableT :: (Applicative f) => ForkableT t f t -> f [t]
 runForkableT (ForkableT act) = runContT act $ pure . (:[])
 
 -- | A general function.
--- Extending it to work on every 'T.Traversable' is more difficult than
--- one would expect.
-forkWith :: (Functor f) => ((a -> f [r]) -> [a] -> f [[r]]) -> [a] -> ForkableT r f a
-forkWith mapper lst = ForkableT $ ContT $ \cont ->
+-- Extending it to work on every 'T.Traversable' (to be "forkTraverse")
+-- is more difficult than one would expect.
+forkWithMap :: (Functor f) => ((a -> f [r]) -> [a] -> f [[r]]) -> [a] -> ForkableT r f a
+forkWithMap mapper lst = ForkableT $ ContT $ \cont ->
   concat <$> mapper cont lst
 
-forkA :: (Functor f, Applicative f) => [a] -> ForkableT r f a
-forkA l = forkWith T.traverse l
+forkForeachA :: (Functor f, Applicative f) => [a] -> ForkableT r f a
+forkForeachA l = forkWithMap T.traverse l
 
 -- | Just a shortcut for concurrent programming, as yo could really
--- use 'forkA' with Control.Concurrent.Async used as an Applicative.
+-- use 'forkForeachA' with Control.Concurrent.Async used as an Applicative.
 -- Requires base monad to be IO. Could be abstracted to accept
 -- instances of MonadIO instead (but would require monad-control or
 -- similar, due to the fact that forkIO only accepts IO actions).
-concFork :: [a] -> ForkableT r IO a
-concFork = forkWith C.mapConcurrently
+forkForeach :: [a] -> ForkableT r IO a
+forkForeach = forkWithMap C.mapConcurrently
 
